05bf251405194823dcaa0d60087837dd5be3f123
kiritokkjiju <79882621+kiritokkjiju@users.noreply.github.com>
2021-03-08 06:12:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
-pkg upgrade
-cd
+pkg install git
+pkg install tesseract
+pkg install nodejs
+pkg install bash
+pkg install wget
+pkg install libwebp
 git clone https://github.com/Darkksinh/bot4.0-
-clear
-cd ./bot4.0-
+cd bot-4.0
 bash install.sh
-cd
-cd ./bot4.0-
 npm start
 
 echo "[*] obrigado por utilizar o bot \"darkbot\" dark agradece"
